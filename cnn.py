d79c0cf771235dcf2a54cc4fa98b80cb4dd586e0
ZeroSakurai <jingjerryyao@outlook.com>
2025-07-10 12:22:01 +0800

    Imports Done

--- cnn.py
+++ cnn.py
@@ -1,1 +1,11 @@
-import torch
+import torch                     # for all things PyTorch
+import torch.nn as nn            # for torch.nn.Module, the parent object for PyTorch models
+import torch.nn.functional as F  # for the activation function
+import torch.optim as optim
+
+import torchvision
+import torchvision.transforms as transforms
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
